e51c55f5ab4fc35ea7f48a68a18f3c9689cff743
Anthony Dibaya <dibayaa@gmail.com>
2021-04-01 17:07:54 -0400

    added cashflow.csv and updated budgetcalc.py

--- BudgetCalc.py
+++ BudgetCalc.py
@@ -1,18 +1,69 @@
-print(''''Hello, this is a Budget Calculator to help you determine
-how lon until you can retire based on your weekly or monthly pay
-and the time when you would like to retire. What is your name?''')
+import pandas as pd 
+import matplotlib.pyplot as plt
 
-input(print('name:'))
+# read in file
+df = pd.read_csv('cashflow.csv', index_col ='Date', 
+parse_dates = True)
+df.head()
+
+#name = input("what is your name: ")
+
+#monthly salary
+#expenses, create a dictionary for expense categories
+
+#dictionary can be necessary expenses and fun stuff and
+#  you can do calculations with the 'fun stuff'
+#
+# dictionary = {'housing': , 'food': ,'clothing': , 'transportation': , 
+# 'fun stuff': , 'childcare/petcare': , 'cellphone': ,'health insurance': ,
+# 'debt': ,'savings': , 'emergency fund': 
+# }
+
+#use pandas to create dataframe
 
 
-def weekly_pay:
-    pass
+#use matplotlib for visualization
+
+# with open('expenses.txt', 'r') as expenses:
+
+#     size_to_read = 10
+
+#     expenses_contents = expenses.read(size_to_read)
+#     print(expenses_contents,end='')
+
+#     expenses.seek(0)
+    
+#     expenses_contents = expenses.read(size_to_read)
+#     print(expenses_contents)
+    
 
 
-def monthly_pay:
-    pass
+    # while len(expenses_contents) > 0:
+    #     print(expenses_contents,end='*')
+    #     expenses_contents = expenses.read(size_to_read)
 
 
-def time_til_retire:
-    pass
+#     fieldnames = ['housing','food','clothing','transportation','fun_stuff','childcare/petcare',
+# 'cellphone','health_insurance','debt','savings','emergency_fund']
+#     expensewriter = csv.DictWriter(csvfile.fieldnames = fieldnames)
+    
+    # expensewriter.writeheader()
+    
+    # expensewriter.writerow({
+    #     # 'housing': '',
+    #     # 'food': ''
+    # })
+  
 
+#store the above in a dataframe
+#export as a csv so user can save their monthly budget
+
+
+#def monthly_pay:
+    #pass
+
+
+#def time_til_retire:
+#    pass
+#time value of money over time..valuation on 
+# investments over time, adjust for salary and expense increase
